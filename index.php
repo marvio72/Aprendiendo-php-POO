87a88790aeec62f46ca89845c86c0c892e2407f1
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-08 00:06:44 -0500

    Escepciones

--- index.php
+++ index.php
@@ -1,37 +1,11 @@
 <?php
 
-class Persona {
-    private $nombre;
-    private $edad;
-    private $ciudad;
-
-    public function __construct($nombre, $edad, $ciudad) {
-        $this->nombre = $nombre;
-        $this->edad = $edad;
-        $this->ciudad = $ciudad;        
+try {
+    if(isset($_GET['id'])){
+        echo "<h1>El parametro es: {$_GET['id']}</h1>";
+    }else{
+        throw new Exception('Faltan parametros por la url');
     }
-
-    public function __call($name, $arguments) {
-        $prefix_metodo = substr($name, 0, 3);
-
-        if($prefix_metodo == "get"){
-            $nombre_metodo = substr(strtolower($name), 3);
-
-            if (!isset($this->$nombre_metodo)) {
-                return "No existe el método";
-            }
-
-            return $this->$nombre_metodo;
-        }else{
-            return "No existe el método";
-        }
-        
-    }
+} catch (Exception $e) {
+    echo "Ha habido un error: ".$e->getMessage();
 }
-
-
-$persona = new Persona("Marco",47,"Guadalajara");
-echo $persona->getNombre();
-echo $persona->getEdad();
-echo $persona->getCiudad();
-echo $persona->getOlivia();
